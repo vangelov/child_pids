91c0bf6a24a7fb93968b046038dd9140dccc957b
Vladi <vlady.angelov@gmail.com>
2016-02-26 22:54:47 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,77 @@
+
+var childProcess = require('child_process'),
+    isRunning = require('is-running');
+
+function find(parentPid, maxDepth, callback) {
+    if(!isRunning(parentPid)) {
+        return process.nextTick(function() {
+            callback(null, new Error('The parent pid could not be found.'));
+        });
+    }
+
+    loadPsInfo(function(err, pids, parentPidMap) {
+        if(err) {
+            return callback(err);
+        }
+
+        var childPids = pids.filter(function(pid) {
+            return isChild(pid, parentPid, maxDepth, parentPidMap);
+        });
+
+        callback(null, childPids);
+    });
+}
+
+function loadPsInfo(callback) {
+    childProcess.exec('ps -A -o pid,ppid', function(err, data) {
+        if(err) {
+            return callback(err);
+        }
+
+        var parentPidMap = [];
+        var pids = [];
+        parsePsOutput(data, pids, parentPidMap);
+
+        callback(null, pids, parentPidMap);
+    });
+}
+
+function parsePsOutput(data, pids, parentPidMap) {
+    var lines = data.split('\n').slice(1);
+
+    lines.forEach(function(line) {
+        var pair = line.trim().split(/\s+/),
+            pid = Number(pair[0]),
+            parentPid = Number(pair[1]);
+
+        parentPidMap[pid] = parentPid;
+        pids.push(pid);
+    });
+}
+
+function isChild(pid, parentPid, maxDepth, parentPidMap) {
+    var nextParentPid = parentPidMap[pid],
+        depth = 1,
+        finalMaxDepth = maxDepth > 0 ?
+                            maxDepth :
+                            Number.POSITIVE_INFINITY;
+
+    while(nextParentPid != undefined && depth <= finalMaxDepth) {
+        if(nextParentPid == parentPid) {
+            return true;
+        }
+
+        nextParentPid = parentPidMap[nextParentPid];
+        depth++;
+    }
+
+    return false;
+}
+
+exports.find = find;
+exports._loadPsInfo = loadPsInfo;
+exports._parsePsOutput = parsePsOutput;
+exports._isChild = isChild;
+
+
+
---
+++ test/test.js
@@ -0,0 +1,55 @@
+var child_pids = require('../index'),
+    chai = require('chai'),
+    expect = chai.expect;
+
+describe('child_pids', function() {
+    var pids, parentPidMap;
+
+    beforeEach(function() {
+        pids = [1, 43, 44, 56];
+
+        parentPidMap = {
+            '1': 0,
+            '43': 1,
+            '44': 1,
+            '56': 44
+        };
+    })
+
+    it('parses ps command output', function() {
+        var output = 'PID  PPID\n  1   0\n43   1\n44   1\n56   44',
+            testParentPidMap = {},
+            testPids = [];
+
+        child_pids._parsePsOutput(output, testPids, testParentPidMap);
+        expect(testPids).to.have.members(pids);
+
+        var parents = testPids.map(function(pid) {
+            return testParentPidMap[pid];
+        });
+
+        expect(parents).to.eql(Object.keys(parentPidMap).map(function(key) {
+            return parentPidMap[key];
+        }));
+    });
+
+    describe('isChild', function() {
+        it('checks children on all levels for maxDepth <= 0', function() {
+            expect(child_pids._isChild(1, 0, 0, parentPidMap)).to.be.true;
+            expect(child_pids._isChild(43, 0, 0, parentPidMap)).to.be.true;
+            expect(child_pids._isChild(56, 0, 0, parentPidMap)).to.be.true;
+        })
+
+        it('checks to a certain when maxDepth > 0', function() {
+            expect(child_pids._isChild(1, 0, 1, parentPidMap)).to.be.true;
+            expect(child_pids._isChild(43, 0, 1, parentPidMap)).to.be.false;
+            expect(child_pids._isChild(56, 1, 2, parentPidMap)).to.be.true;
+            expect(child_pids._isChild(56, 1, 1, parentPidMap)).to.be.false;
+        })
+
+        it('fails when there is no common parent', function() {
+            expect(child_pids._isChild(43, 44, 0, parentPidMap)).to.be.false;
+            expect(child_pids._isChild(56, 43, 0, parentPidMap)).to.be.false;
+        });
+    })
+});
